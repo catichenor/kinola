4186d57ba171776053a86be415f2fb96dfd4e108
Christopher Tichenor <catichenor@gmail.com>
2014-02-08 14:46:27 -0800

    Test to receive a sequence of commands.

--- autokeyboard.ino
+++ autokeyboard.ino
@@ -6,12 +6,20 @@
 
  Possible commands created in this shetch:
 
- * "/arduino/type/hello%20world"     -> types "hello world" as a   
-                                        keyboard through the USB port
-                                        on a connected computer.
- * "/arduino/wait/10                 -> turns on the LED, waits 
-                                        10 seconds, then turns the
-                                        LED off
+ * "/arduino/type/hello%20world"      -> types "hello world" as a   
+                                         keyboard through the USB port
+                                         on a connected computer.
+ * "/arduino/keys/h128/h130/p212/r-1" -> holds the Left Control key, 
+                                         then holds the Left Alt key,
+                                         then presses the Delete key, 
+                                         then releases all keys
+ * "/arduino/wait/10                  -> turns on the LED, waits 
+                                         10 seconds, then turns the
+                                         LED off
+                                        
+ For additional reference on Key Codes 
+ (used for the keys function) see:
+ http://arduino.cc/en/Reference/KeyboardModifiers
 
  This project has been uploaded to:
 
@@ -62,6 +70,11 @@ void process(YunClient client) {
   // is "type" command?
   if (command == "type") {
     typeCommand(client);
+  }  
+  
+  // is "keys" command?
+  if (command == "keys") {
+    keysCommand(client);
   }
 
   // is "wait" command?
@@ -79,6 +92,70 @@ void typeCommand(YunClient client) {
   delay(1000);
   Keyboard.end();
   digitalWrite(13, LOW);
+}
+
+/*
+void keysCommand(YunClient client) {
+  String entry;
+  char key;
+  
+  while (client.read == '/') {
+    entry = client.readStringUntil('/');
+    key = entry.substring(1,entry.length()).toInt();
+  }
+
+  // Read seconds
+  secs = client.parseInt();
+
+  secs = secs * 1000;
+  
+  digitalWrite(13, HIGH);
+  delay(secs);
+  digitalWrite(13, LOW);
+}
+*/
+
+void keysCommand(YunClient client) {
+  int delimiter;
+  String command;
+  int sequence;
+  Keyboard.begin();
+  String entry = client.readStringUntil('\r');
+  String message = entry;
+  Keyboard.println(entry);
+  do {
+    delay(2000);
+    delimiter = message.indexOf('/');
+    if(delimiter != -1) {
+      command = message.substring(0,delimiter);
+      Keyboard.println(command);
+      sequence = command.toInt();
+      Keyboard.println(sequence);
+      message = message.substring(delimiter+1, message.length());
+      Keyboard.println(message);
+      int i = 0;
+      for(i=0;i<sequence;i++) {
+        digitalWrite(13, HIGH);
+        delay(200);
+        digitalWrite(13, LOW);
+        delay(200);
+      }
+    }
+    else {
+      if (message.length() > 0) {
+        sequence = message.toInt();
+        int i;
+        for(i=0;i<sequence;i++) {
+          digitalWrite(13, HIGH);
+          delay(200);
+          digitalWrite(13, LOW);
+          delay(200);
+        }
+      }
+    }   
+  }
+  while(delimiter >=0);
+  Keyboard.end();
 }
 
 void waitCommand(YunClient client) {
