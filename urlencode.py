5db562b4f8aa0588af6d73c56c6149b8b79af34d
Christopher Tichenor <catichenor@gmail.com>
2014-02-10 23:14:40 -0800

    Added key commands to the Python script.

--- urlencode.py
+++ urlencode.py
@@ -1,5 +1,27 @@
+import argparse
 import urllib
-import sys
+import time
 
-theOutput = sys.argv[1]
-print urllib.quote(theOutput)
+parser = argparse.ArgumentParser()
+
+parser.add_argument("address", help="IP address of the Arduino Yun")
+parser.add_argument("text", help="the text or \(with -c\), the set of commands to send")
+parser.add_argument("-c", "--command", action="store_true", help="send as a set of keyboard commands")
+
+args = parser.parse_args()
+
+# If the -c switch is used, interpret the input text as a sequence of commands.
+
+if args.command:
+	outCommand = args.text #Pass the commands directly
+	outKind = "keys"
+else:
+	outCommand = urllib.quote(args.text) #URI encode the text to be sent
+	outKind = "type"
+
+# Insert a delay for local testing. Comment for instant gratification.
+time.sleep(3)
+
+# Put it all together, send the request.
+urllib.urlopen("http://" + args.address + "/arduino/" + outKind + "/" + outCommand)
+
